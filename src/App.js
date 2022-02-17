430f655c095d93b88bae02432d2be8b7c7fb04f3
MEYHOUSE\Tim.MacLachlan <tim.maclachlan@morningdata.co.uk>
2022-02-17 11:22:15 +0000

    Initial commit - set up dashboard layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>Foobar Publishing</header>
+      <section>
+        <nav>
+          <ul>
+            <li>Authors</li>
+            <li>Books</li>
+            <li>Sales</li>
+            <li>Royalties</li>
+          </ul>
+        </nav>
+        <main>
+          <Dashboard />
+        </main>
+      </section>
+      <footer>
+        <p>&copy;2022</p>
+      </footer>      
+      </>
   );
 }
 
