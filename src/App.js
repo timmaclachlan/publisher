b09c60cc9990ee132a319e89847132e9f16f81d7
MEYHOUSE\Tim.MacLachlan <tim.maclachlan@morningdata.co.uk>
2022-02-17 15:23:51 +0000

    Adding React Router v6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 import './App.css';
 
 import Dashboard from './components/Dashboard';
+import Authors from './components/Authors';
+import Books from './components/Books';
 
 function App() {
   return (
@@ -9,20 +19,25 @@ function App() {
       <section>
         <nav>
           <ul>
-            <li>Authors</li>
-            <li>Books</li>
-            <li>Sales</li>
-            <li>Royalties</li>
+            <li><Link to="/authors">Authors</Link></li>
+            <li><Link to="/books">Books</Link></li>
+            <li><Link to="/sales">Sales</Link></li>
+            <li><Link to="/royalties">Royalties</Link></li>
           </ul>
         </nav>
         <main>
-          <Dashboard />
+          <Routes>
+              <Route index element={<Dashboard />} />
+              <Route path="authors" element={<Authors />} />
+              <Route path="books" element={<Books />} />
+          </Routes>            
         </main>
       </section>
       <footer>
         <p>&copy;2022</p>
       </footer>      
       </>
+
   );
 }
 
