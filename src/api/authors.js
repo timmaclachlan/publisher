785a0bc5e0f53b3ef3839a695dee6f42788e02db
MEYHOUSE\Tim.MacLachlan <tim.maclachlan@morningdata.co.uk>
2022-02-28 17:38:29 +0000

    adding update / create for authors

--- src/api/authors.js
+++ src/api/authors.js
@@ -16,4 +16,24 @@ export const getAuthorById = (id) => new Promise((resolve, reject) => {
   setTimeout(() => resolve(author), 500);
 })
   
+export const updateAuthor = (data) => new Promise((resolve, reject) => {
+  let index = authors.findIndex(x => x.id === parseInt(data.id));
+  if (index === -1) {
+    reject(new Error('Author not found'));
+  }
+  authors[index] = { ...authors[index], ...data };
+
+  setTimeout(() => resolve(true), 500);
+})
+
+export const createAuthor = (data) => new Promise((resolve, reject) => {
+  if (!data.name) {
+    reject(new Error('Name should be provided'));
+  }
+  debugger;
+  const maxId = Math.max.apply(Math, authors.map(x => x.id));
+  authors.push({ ...data, id: maxId + 1 });
+
+  setTimeout(() => resolve(maxId + 1), 500);
+})
 
--- src/components/AuthorDetail.js
+++ src/components/AuthorDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState} from 'react'
 
 import { useParams } from 'react-router-dom'
 
-import { getAuthorById } from '../api/authors';
+import { getAuthorById, updateAuthor, createAuthor } from '../api/authors';
 
 const AuthorDetail = () => {
   const { id } = useParams();
@@ -32,12 +32,33 @@ const AuthorDetail = () => {
     });
   }
 
+  const updateClick = (event) => {
+    event.preventDefault();
+
+     const saveAuthor = async () => {
+       const result = await updateAuthor(author);
+     }
+    saveAuthor();
+  }
+
+  const createClick = (event) => {
+    event.preventDefault();
+
+    const saveAuthor = async () => {
+      const result = await createAuthor(author);      
+    }
+    saveAuthor();
+  }
+
   return (
     <form>
       <label htmlFor="name">Name</label>
       <input type="text" name="name" value={author.name} onChange={handleChange} />
       <label htmlFor="address">Address</label>
       <input type="text" name="address" value={author.address} onChange={handleChange} />
+
+      <button onClick={updateClick}>Update</button>
+      <button onClick={createClick}>Create</button>
     </form>
   )
 }
