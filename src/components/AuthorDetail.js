63278b7b8ad765a2d842937b938f7e09f6b9324f
MEYHOUSE\Tim.MacLachlan <tim.maclachlan@morningdata.co.uk>
2022-03-25 17:45:15 +0000

    author edit mode

--- src/components/AuthorDetail.js
+++ src/components/AuthorDetail.js
@@ -1,32 +1,30 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams, useNavigate } from "react-router-dom";
 
 import { readById, updateById, deleteById, create } from "../fetcher";
 
 const AuthorDetail = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  const [author, setAuthor] = useState({id: 0, name: '', address: ''});
+  const [author, setAuthor] = useState({ id: 0, name: "", address: "" });
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     const retrieveAuthor = async () => {
       try {
-        const authorRecord = await readById('author', id);
+        const authorRecord = await readById("author", id);
         setAuthor(authorRecord.data.author);
-      }
-      catch (error) {
+      } catch (error) {
         console.log(error);
-        navigate('/notfound');
+        navigate("/notfound");
       }
-    }
+    };
     if (id > 0) {
       retrieveAuthor();
+    } else if (id !== undefined) {
+      navigate("/notfound");
     }
-    else if (id !== undefined) {
-      navigate('/notfound');
-    }
-
   }, [id, navigate]);
 
   const handleChange = (event) => {
@@ -35,42 +33,79 @@ const AuthorDetail = () => {
     setAuthor((prevState) => {
       return {
         ...prevState,
-        [name]: value
-      }
+        [name]: value,
+      };
     });
-  }
+  };
 
-  const callApi = (event, method) => {
+  const makeChange = (event, method) => {
     event.preventDefault();
-    const callApi = async () => {    
-       await method('author');
-     }
-    callApi();
-  }
+    setEditMode(false);
 
+    const callApi = async () => {
+      await method("author");
+    };
+    callApi();
+  };
 
   return (
     <div>
-    <form>
-      <label htmlFor="name">Name</label>
-      <input type="text" name="name" value={author.name} onChange={handleChange} />
-      <label htmlFor="address">Address</label>
-      <input type="text" name="address" value={author.address} onChange={handleChange} />
+      <section className="authorDetailsContainer">
+        {!editMode && (
+          <section className="authorDetails">
+            <button
+                className="authorDetailsDeleteButton"
+                onClick={ev => makeChange(ev, deleteById.bind(null, id))}
+              >Delete</button>
+            <button className="authorDetailsEditButton" onClick={() => setEditMode(true)}>
+              Edit
+            </button>
+            <label>Name</label>
+            <label className="authorDetails">{author.name}</label>
+            <label>Address</label>
+            <label className="authorDetails">{author.address}</label>
+          </section>
+        )}
 
-      {author.id === 0 &&
-        <button onClick={ev => callApi(ev, create.bind(null, author))}>Create</button>
-      }
-      {author.id > 0 &&
-        <button onClick={ev => callApi(ev, updateById.bind(null, author, id))}>Update</button>
-      }
+        {editMode && (
+          <form>
+            <section className="authorDetails">
+              <button className="authorDetailsCancelButton" 
+              onClick={() => setEditMode(false)}>Cancel</button>
+              <button
+                className="authorDetailsSaveButton"
+                onClick={ev => makeChange(ev, updateById.bind(null, author, id))}
+              >
+                Save
+              </button>
 
-      <button onClick={ev => callApi(ev, deleteById.bind(null, id))}>Delete</button>
-      </form>    
-      <div><button onClick={() => navigate('/authors')}>
-        Authors
-      </button></div>
-    </div>      
-  )
-}
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                value={author.name}
+                onChange={handleChange}
+                className="authorDetails"
+              />
 
-export default AuthorDetail
+              <label htmlFor="address">Address</label>
+              <input
+                type="text"
+                name="address"
+                value={author.address}
+                onChange={handleChange}
+                className="authorDetails"
+              />
+            </section>
+          </form>
+        )}
+      </section>
+
+      <div>
+        <button onClick={() => navigate("/authors")}>Authors</button>
+      </div>
+    </div>
+  );
+};
+
+export default AuthorDetail;
