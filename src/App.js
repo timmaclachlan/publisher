432f45937c3eb8530ff3d47fc3fa4f7a2abf4158
MEYHOUSE\Tim.MacLachlan <tim.maclachlan@morningdata.co.uk>
2022-04-01 15:22:46 +0100

    Get positioning working without forwardRef

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import Drawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
@@ -17,37 +18,31 @@ import BooksPage from "./components/BooksPage";
 import BookDetailPage from "./components/BookDetailPage";
 import NotFound from "./components/NotFound";
 
-
-function DashboardContent() {
+function App() {
   return (
+    <Box>
+      <CssBaseline />
+      <AppBar>
+        <Toolbar></Toolbar>
+      </AppBar>
+      <Drawer variant="permanent">
+        <Toolbar />
+        <List component="nav">{mainListItems}</List>
+      </Drawer>
 
-      <Box >
-        <CssBaseline />
-        <AppBar>
-          <Toolbar>
-          </Toolbar>
-        </AppBar>
-        <Drawer variant="permanent">          
-          <List component="nav">{mainListItems}</List>
-        </Drawer>
-        <Box>
-          <Toolbar />
-
-          <Routes>
-            <Route index element={<Dashboard />} />
-            <Route path="authors" element={<AuthorsPage />} />
-            <Route path="authors/:id" element={<AuthorDetailPage />} />
-            <Route path="authors/new" element={<AuthorDetailPage />} />
-            <Route path="books" element={<BooksPage />} />
-            <Route path="books/:id" element={<BookDetailPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Box>
-      </Box>
-
+      <Container sx={{ marginTop: "80px", marginLeft: "180px" }}>
+        <Routes>
+          <Route index element={<Dashboard />} />
+          <Route path="authors" element={<AuthorsPage />} />
+          <Route path="authors/:id" element={<AuthorDetailPage />} />
+          <Route path="authors/new" element={<AuthorDetailPage />} />
+          <Route path="books" element={<BooksPage />} />
+          <Route path="books/:id" element={<BookDetailPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
+    </Box>
   );
 }
 
-export default function App() {
-  return <DashboardContent />;
-}
+export default App;
--- src/listItems.js
+++ src/listItems.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-
+import { Link as RouterLink } from "react-router-dom";
 
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -13,11 +13,14 @@ import BarChartIcon from "@mui/icons-material/BarChart";
 import LayersIcon from "@mui/icons-material/Layers";
 
 function ListItemLink(props) {
-  const { icon, primary } = props;
+  const { icon, to, primary } = props;
 
+  const renderLink = (itemProps) => {
+    return <RouterLink to={to} {...itemProps}>{itemProps.children}</RouterLink>;
+  };
 
   return (
-    <ListItemButton>
+    <ListItemButton component={renderLink}>
       {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
       <ListItemText primary={primary} />
     </ListItemButton>
@@ -26,10 +29,10 @@ function ListItemLink(props) {
 
 export const mainListItems = (
   <React.Fragment>
-    <ListItemLink primary="Dashboard" icon={<DashboardIcon />} />
-    <ListItemLink primary="Sales" icon={<ShoppingCartIcon />} />
-    <ListItemLink primary="Authors" icon={<PeopleIcon />} />
-    <ListItemLink primary="Books" icon={<LayersIcon />} />
-    <ListItemLink primary="Royalties" icon={<BarChartIcon />} />
+    <ListItemLink to="/" primary="Dashboard" icon={<DashboardIcon />} />
+    <ListItemLink to="/sales" primary="Sales" icon={<ShoppingCartIcon />} />
+    <ListItemLink to="/authors" primary="Authors" icon={<PeopleIcon />} />
+    <ListItemLink to="/books/" primary="Books" icon={<LayersIcon />} />
+    <ListItemLink to="/royalties" primary="Royalties" icon={<BarChartIcon />} />
   </React.Fragment>
 );
