70459432e09c71e94aadf31c0d34273c555ae0ab
MEYHOUSE\Tim.MacLachlan <tim.maclachlan@morningdata.co.uk>
2022-04-11 11:09:12 +0100

    Separate edit form to component

---
+++ src/components/BookDetail/BookEdit.js
@@ -0,0 +1,334 @@
+import React from "react";
+
+import {
+  Grid,
+  TextField,
+  Select,
+  Button,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  Checkbox,
+  FormControlLabel,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Autocomplete,
+  InputAdornment,
+  Card,
+  CardContent,
+  FormGroup,
+  Switch,
+  Typography,
+  Stack,
+} from "@mui/material";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import LayersIcon from "@mui/icons-material/Layers";
+import CancelIcon from "@mui/icons-material/Cancel";
+import SaveIcon from "@mui/icons-material/Save";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+
+const BookEdit = ({ book, onUpdateBook, onUpdateEditMode, onSaveBook }) => {
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    onUpdateBook((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleDatePickerChange = (value) => {
+    onUpdateBook((prevState) => {
+      return {
+        ...prevState,
+        publicationDate: value,
+      };
+    });
+  };
+
+  const authors = [{ label: "Enid Blyton" }, { label: "John Doe" }];
+
+  return (
+    <form>
+      <Grid container spacing={2}>
+        <Grid item md={1}>
+          <LayersIcon color="primary" sx={{ fontSize: 60, mr: 2 }} />
+        </Grid>
+        <Grid item md={3}>
+          <Typography variant="h4" sx={{ pt: 1 }}>
+            Edit Book
+          </Typography>
+        </Grid>
+        <Grid item md={2} />
+        <Grid item md={2}>
+          <Button
+            variant="outlined"
+            startIcon={<CancelIcon />}
+            onClick={() => onUpdateEditMode(false)}
+          >
+            Cancel
+          </Button>
+        </Grid>
+
+        <Grid item md={2}>
+          <Button
+            variant="contained"
+            color="error"
+            startIcon={<DeleteIcon />}
+            onClick={() => onUpdateEditMode(false)}
+          >
+            Delete
+          </Button>
+        </Grid>
+
+        <Grid item md={2}>
+          <Button
+            variant="contained"
+            startIcon={<SaveIcon />}
+            color="success"
+            sx={{ width: "100px" }}
+            onClick={onSaveBook}
+          >
+            Save
+          </Button>
+        </Grid>
+
+        <Grid item md={9}>
+          <Grid container spacing={2}>
+            <Grid item md={9}>
+              <TextField
+                label="Title"
+                name="title"
+                variant="outlined"
+                value={book.title}
+                fullWidth
+                onChange={handleChange}
+              />
+            </Grid>
+
+            <Grid item md={3}>
+              <FormControlLabel
+                control={<Switch defaultChecked />}
+                label="Published"
+              />
+            </Grid>
+
+            <Grid item md={4}>
+              <Autocomplete
+                options={authors}
+                disablePortal
+                renderInput={(params) => (
+                  <TextField {...params} label="Author" />
+                )}
+              />
+            </Grid>
+
+            <Grid item md={3}>
+              <TextField
+                label="Genre"
+                name="genre"
+                variant="outlined"
+                value={book.genre}
+                onChange={handleChange}
+              />
+            </Grid>
+
+            <Grid item md={2}>
+              <FormControl>
+                <InputLabel id="service-label">Service</InputLabel>
+                <Select
+                  label="Service"
+                  name="service"
+                  value={book.service}
+                  labelId="service-label"
+                  onChange={handleChange}
+                >
+                  <MenuItem value="EPPS">EPPS</MenuItem>
+                  <MenuItem value="PPS">PPS</MenuItem>
+                  <MenuItem value="EPS">EPS</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+
+            <Grid item md={3}>
+              <DesktopDatePicker
+                label="Publication Date"
+                name="publicationDate"
+                value={book.publicationDate}
+                onChange={handleDatePickerChange}
+                inputFormat="dd/MM/yyyy"
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </Grid>
+
+            <Grid item md={4}>
+              <Card>
+                <CardContent>
+                  <FormGroup>
+                    <Stack spacing={2}>
+                      <FormControlLabel
+                        label="Paperback"
+                        labelPlacement="end"
+                        control={<Checkbox defaultChecked />}
+                      />
+                      <TextField
+                        name="pricePaperback"
+                        label="Price"
+                        variant="outlined"
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">£</InputAdornment>
+                          ),
+                        }}
+                      />
+                      <TextField
+                        name="isbnPaperback"
+                        label="ISBN"
+                        variant="outlined"
+                      />
+                    </Stack>
+                  </FormGroup>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item md={4}>
+              <Card>
+                <CardContent>
+                  <FormGroup>
+                    <Stack spacing={2}>
+                      <FormControlLabel
+                        label="Hardback"
+                        labelPlacement="end"
+                        control={<Checkbox defaultChecked />}
+                      />
+                      <TextField
+                        name="priceHardback"
+                        label="Price"
+                        variant="outlined"
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">£</InputAdornment>
+                          ),
+                        }}
+                      />
+                      <TextField
+                        name="isbnHardback"
+                        label="ISBN"
+                        variant="outlined"
+                      />
+                    </Stack>
+                  </FormGroup>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item md={4}>
+              <Card>
+                <CardContent>
+                  <FormGroup>
+                    <Stack spacing={2}>
+                      <FormControlLabel
+                        label="E-Book"
+                        labelPlacement="end"
+                        control={<Checkbox defaultChecked />}
+                      />
+                      <TextField
+                        name="pricePaperback"
+                        label="Price"
+                        variant="outlined"
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">£</InputAdornment>
+                          ),
+                        }}
+                      />
+                      <TextField
+                        name="isbnPaperback"
+                        label="ISBN"
+                        variant="outlined"
+                      />
+                    </Stack>
+                  </FormGroup>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item md={3}>
+          <Grid container spacing={2}>
+            <Grid item md={12}>
+              <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                  Royalties
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Stack spacing={2} direction="row">
+                    <TextField
+                      label="Author"
+                      name="royaltyAuthor"
+                      variant="outlined"
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">%</InputAdornment>
+                        ),
+                      }}
+                    />
+                    <TextField
+                      label="Publisher"
+                      name="royaltyPublisher"
+                      variant="outlined"
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">%</InputAdornment>
+                        ),
+                      }}
+                    />
+                  </Stack>
+                </AccordionDetails>
+              </Accordion>
+            </Grid>
+
+            <Grid item md={12}>
+              <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                  Options
+                </AccordionSummary>
+                <AccordionDetails>
+                  <FormGroup>
+                    <FormControlLabel
+                      label="Still Selling"
+                      labelPlacement="start"
+                      control={<Checkbox defaultChecked />}
+                    />
+                    <FormControlLabel
+                      label="Terminated"
+                      labelPlacement="start"
+                      control={<Checkbox defaultChecked />}
+                    />
+                    <FormControlLabel
+                      label="On Hold"
+                      labelPlacement="start"
+                      control={<Checkbox defaultChecked />}
+                    />
+                    <FormControlLabel
+                      label="Mature Content"
+                      labelPlacement="start"
+                      control={<Checkbox defaultChecked />}
+                    />
+                  </FormGroup>
+                </AccordionDetails>
+              </Accordion>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default BookEdit;
--- src/components/BookDetailPage.js
+++ src/components/BookDetailPage.js
@@ -5,43 +5,21 @@ import { useParams, Link, useNavigate } from "react-router-dom";
 import {
   Grid,
   Box,
-  TextField,
-  Select,
   Button,
-  MenuItem,
-  InputLabel,
-  FormControl,
-  Checkbox,
-  FormControlLabel,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Autocomplete,
-  InputAdornment,
   Card,
   CardMedia,
   CardContent,
   Input,
-  OutlinedInput,
-  Chip,
-  FormGroup,
-  FormLabel,
-  Switch,
   Typography,
-  Stack,
-  Avatar,
 } from "@mui/material";
 
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import LayersIcon from "@mui/icons-material/Layers";
 import EditIcon from "@mui/icons-material/Edit";
 import CancelIcon from "@mui/icons-material/Cancel";
-import SaveIcon from "@mui/icons-material/Save";
-import DeleteIcon from "@mui/icons-material/Delete";
 
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import BookEdit from "./BookDetail/BookEdit";
 
-import { readById, updateById, deleteById } from "../fetcher";
+import { readById, updateById } from "../fetcher";
 
 const BookDetail = ({ onRecordChange }) => {
   const { id } = useParams();
@@ -61,40 +39,21 @@ const BookDetail = ({ onRecordChange }) => {
       }
     };
     retrieveBook();
-  }, [id]);
+  }, [id, onRecordChange]);
 
-  const handleChange = (event) => {
+  const makeChange = (method) => {
     debugger;
-    const { name, value } = event.target;
-    setBook((prevState) => {
-      return {
-        ...prevState,
-        [name]: value,
-      };
-    });
-  };
-
-  const handleDatePickerChange = (value) => {
-    debugger;
-    setBook((prevState) => {
-      return {
-        ...prevState,
-        publicationDate: value,
-      };
-    });
-  };
-
-  const makeChange = (event, method) => {
-    event.preventDefault();
-    setEditMode(false);
-
     const callApi = async () => {
       await method("book");
     };
     callApi();
   };
 
-  const authors = [{ label: "Enid Blyton" }, { label: "John Doe" }];
+  const saveBook = (ev) => {
+    ev.preventDefault();    
+    debugger;
+    makeChange(updateById.bind(null, book, id))
+  }
 
   return (
     <>
@@ -158,293 +117,13 @@ const BookDetail = ({ onRecordChange }) => {
             )}
 
             {editMode && (
-              <form>
-                <Grid container spacing={2}>
-                  <Grid item md={1}>
-                    <LayersIcon color="primary" sx={{ fontSize: 60, mr: 2 }} />
-                  </Grid>
-                  <Grid item md={3}>
-                    <Typography variant="h4" sx={{ pt: 1 }}>
-                      Edit Book
-                    </Typography>
-                  </Grid>
-                  <Grid item md={2} />
-                  <Grid item md={2}>
-                    <Button
-                      variant="outlined"
-                      startIcon={<CancelIcon />}
-                      onClick={() => setEditMode(false)}
-                    >
-                      Cancel
-                    </Button>
-                  </Grid>
-
-                  <Grid item md={2}>
-                    <Button
-                      variant="contained"
-                      color="error"
-                      startIcon={<DeleteIcon />}
-                      onClick={() => setEditMode(false)}
-                    >
-                      Delete
-                    </Button>
-                  </Grid>
-
-                  <Grid item md={2}>
-                    <Button
-                      variant="contained"
-                      startIcon={<SaveIcon />}
-                      color="success"
-                      sx={{ width: "100px" }}
-                      onClick={() => setEditMode(true)}
-                    >
-                      Save
-                    </Button>
-                  </Grid>
-
-                  <Grid item md={9}>
-                    <Grid container spacing={2}>
-                      <Grid item md={9}>
-                        <TextField
-                          label="Title"
-                          name="title"
-                          variant="outlined"
-                          value={book.title}
-                          fullWidth
-                          onChange={handleChange}
-                        />
-                      </Grid>
-
-                      <Grid item md={3}>
-                        <FormControlLabel
-                          control={<Switch defaultChecked />}
-                          label="Published"
-                        />
-                      </Grid>
-
-                      <Grid item md={4}>
-                        <Autocomplete
-                          options={authors}
-                          disablePortal
-                          renderInput={(params) => (
-                            <TextField {...params} label="Author" />
-                          )}
-                        />
-                      </Grid>
-
-                      <Grid item md={3}>
-                        <TextField
-                          label="Genre"
-                          name="genre"
-                          variant="outlined"
-                          value={book.genre}
-                          onChange={handleChange}
-                        />
-                      </Grid>
-
-                      <Grid item md={2}>
-                        <FormControl>
-                          <InputLabel id="service-label">Service</InputLabel>
-                          <Select
-                            label="Service"
-                            name="service"
-                            value={book.service}
-                            labelId="service-label"
-                            onChange={handleChange}
-                          >
-                            <MenuItem value="EPPS">EPPS</MenuItem>
-                            <MenuItem value="PPS">PPS</MenuItem>
-                            <MenuItem value="EPS">EPS</MenuItem>
-                          </Select>
-                        </FormControl>
-                      </Grid>
-
-                      <Grid item md={3}>
-                        <DesktopDatePicker
-                          label="Publication Date"
-                          name="publicationDate"
-                          value={book.publicationDate}
-                          onChange={handleDatePickerChange}
-                          inputFormat="dd/MM/yyyy"
-                          renderInput={(params) => <TextField {...params} />}
-                        />
-                      </Grid>
-
-                      <Grid item md={4}>
-                        <Card>
-                          <CardContent>
-                            <FormGroup>
-                              <Stack spacing={2}>
-                              <FormControlLabel
-                                label="Paperback"
-                                labelPlacement="end"
-                                control={<Checkbox defaultChecked />}
-                              />
-                              <TextField 
-                                name="pricePaperback"
-                                label="Price"
-                                  variant="outlined"
-                                  InputProps={{
-                                  startAdornment: (
-                                    <InputAdornment position="start">
-                                      £
-                                    </InputAdornment>
-                                  ),
-                                }}
-                              />
-                              <TextField 
-                                name="isbnPaperback"
-                                label="ISBN"
-                                variant="outlined"
-                                
-                                />
-                                </Stack>
-                            </FormGroup>
-                          </CardContent>
-                        </Card>
-                      </Grid>
-                      <Grid item md={4}>
-                        <Card>
-                          <CardContent>
-                            <FormGroup>
-                              <Stack spacing={2}>
-                              <FormControlLabel
-                                label="Hardback"
-                                labelPlacement="end"
-                                control={<Checkbox defaultChecked />}
-                              />
-                              <TextField 
-                                name="priceHardback"
-                                label="Price"
-                                  variant="outlined"
-                                  InputProps={{
-                                  startAdornment: (
-                                    <InputAdornment position="start">
-                                      £
-                                    </InputAdornment>
-                                  ),
-                                }}
-                              />
-                              <TextField 
-                                name="isbnHardback"
-                                label="ISBN"
-                                variant="outlined"
-                                
-                                />
-                                </Stack>
-                            </FormGroup>
-                          </CardContent>
-                        </Card>
-                      </Grid>
-                      <Grid item md={4}>
-                        <Card>
-                          <CardContent>
-                            <FormGroup>
-                              <Stack spacing={2}>
-                              <FormControlLabel
-                                label="E-Book"
-                                labelPlacement="end"
-                                control={<Checkbox defaultChecked />}
-                              />
-                              <TextField 
-                                name="pricePaperback"
-                                label="Price"
-                                  variant="outlined"
-                                  InputProps={{
-                                  startAdornment: (
-                                    <InputAdornment position="start">
-                                      £
-                                    </InputAdornment>
-                                  ),
-                                }}
-                              />
-                              <TextField 
-                                name="isbnPaperback"
-                                label="ISBN"
-                                variant="outlined"
-                                
-                                />
-                                </Stack>
-                            </FormGroup>
-                          </CardContent>
-                        </Card>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-
-                  <Grid item md={3}>
-                    <Grid container spacing={2}>
-                      <Grid item md={12}>
-                        <Accordion>
-                          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                            Royalties
-                          </AccordionSummary>
-                          <AccordionDetails>
-                            <Stack spacing={2} direction="row">
-                              <TextField
-                                label="Author"
-                                name="royaltyAuthor"
-                                variant="outlined"
-                                InputProps={{
-                                  startAdornment: (
-                                    <InputAdornment position="start">
-                                      %
-                                    </InputAdornment>
-                                  ),
-                                }}
-                              />
-                              <TextField
-                                label="Publisher"
-                                name="royaltyPublisher"
-                                variant="outlined"
-                                InputProps={{
-                                  startAdornment: (
-                                    <InputAdornment position="start">
-                                      %
-                                    </InputAdornment>
-                                  ),
-                                }}
-                              />
-                            </Stack>
-                          </AccordionDetails>
-                        </Accordion>
-                      </Grid>
-
-                      <Grid item md={12}>
-                        <Accordion>
-                          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                            Options
-                          </AccordionSummary>
-                          <AccordionDetails>
-                            <FormGroup>
-                              <FormControlLabel
-                                label="Still Selling"
-                                labelPlacement="start"
-                                control={<Checkbox defaultChecked />}
-                              />
-                              <FormControlLabel
-                                label="Terminated"
-                                labelPlacement="start"
-                                control={<Checkbox defaultChecked />}
-                              />
-                              <FormControlLabel
-                                label="On Hold"
-                                labelPlacement="start"
-                                control={<Checkbox defaultChecked />}
-                              />
-                              <FormControlLabel
-                                label="Mature Content"
-                                labelPlacement="start"
-                                control={<Checkbox defaultChecked />}
-                              />
-                            </FormGroup>
-                          </AccordionDetails>
-                        </Accordion>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </form>
+              <BookEdit
+                book={book}
+                id={id}
+                onUpdateBook={setBook}
+                onUpdateEditMode={setEditMode}
+                onSaveBook={saveBook}
+              />
             )}
           </Grid>
 
