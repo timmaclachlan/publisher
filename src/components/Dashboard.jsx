0ccd2a2c15b4b62b7e530a304b8811200ce694d7
MEYHOUSE\Tim.MacLachlan <tim.maclachlan@morningdata.co.uk>
2022-05-04 11:01:41 +0100

    WIP

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,64 @@
-import React from 'react'
-import { Button } from '@mui/material'
+import React from "react";
+
+import { Button, Stack, TextField } from "@mui/material";
+
+import dbpool from "../postgres";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard
-      <Button variant="contained" >Hello World</Button>
-    </div>
-  )
-}
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-export default Dashboard
+  const handleUserName = (ev) => {
+    setUsername(ev.target.value);
+  };
+
+  const handlePassword = (ev) => {
+    setUsername(ev.target.value);
+  };
+
+  const getAuthorsPostgres = () => {
+    return new Promise(function (resolve, reject) {
+      dbpool.query("SELECT * FROM authors", (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        debugger;
+        resolve(results.rows);
+      });
+    });
+  };
+
+  const handleLogin = (ev) => {
+    ev.preventDefault();
+
+    debugger;
+    const getAuthors = async () => {
+      debugger;
+      const authors = await getAuthorsPostgres();
+      return authors;
+    };
+    getAuthors();
+  };
+
+  return (
+    <div>
+      Dashboard
+      <Button variant="contained">Hello World</Button>
+      <Stack>
+        <TextField
+          variant="contained"
+          name="username"
+          handleChange={handleUserName}
+        />
+        <TextField
+          variant="contained"
+          name="password"
+          handleChange={handlePassword}
+        />
+        <Button onClick={handleLogin}>Login</Button>
+      </Stack>
+    </div>
+  );
+};
+
+export default Dashboard;
