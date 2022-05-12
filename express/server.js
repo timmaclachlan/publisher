83410ea863544bb0abef68c67a0d155b8b251783
MEYHOUSE\Tim.MacLachlan <tim.maclachlan@morningdata.co.uk>
2022-05-12 13:43:23 +0100

    add pool back in

--- express/server.js
+++ express/server.js
@@ -1,47 +1,65 @@
-const express = require('express');
-const serverless = require('serverless-http');
+const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const Pool = require("pg").Pool;
 const v4 = require("uuid").v4;
-const config = require('dotenv').config();
+const config = require("dotenv").config();
 
 const DBPATH = "timm2006/athena";
 const TABLE = "authors";
 const TABLEQUAL = `"${DBPATH}"."${TABLE}"`;
 
 const postgresConfig = {
-  user: 'timm2006_demo_db_connection',
-  host: 'db.bit.io',
-  database: 'bitdotio',
-  password: '3nEd6_jhhm4h3xLRwbUbcQJQQSYX4',
-  port: 5432
-}
+  user: "timm2006_demo_db_connection",
+  host: "db.bit.io",
+  database: "bitdotio",
+  password: "3nEd6_jhhm4h3xLRwbUbcQJQQSYX4",
+  port: 5432,
+};
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+
+router.post("/authors", (req, res) => {
+  let sql =
+    `INSERT INTO ${TABLEQUAL} (id, realName,penName) VALUES($1,$2,$3) RETURNING id`;
+  
+  let data = [v4(), req.body.realName, req.body.penName];
+
+  const pool = new Pool();
+
+  pool.query(sql, data, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.statusCode = 500;
+      return res.json({ errors: ["Failed to create record:" + error] });
+    }
+    res.statusCode = 201;
+    res.json({ message: "success", id: results.rows[0].id });
+  });
 });
 
-router.get('/authors', (req, res) => {
+router.get("/authors", (req, res) => {
   let sql = `SELECT * FROM ${TABLEQUAL}`;
 
   console.log(sql);
   console.log(process.env.PGUSER);
 
-  res.json({ pguserenv: process.env.PGUSER, pguser: postgresConfig.user });
+  //res.json({ pguserenv: process.env.PGUSER, pguser: postgresConfig.user });
 
-  // const pool = new Pool();
-  // pool.query(sql, (error, results) => {
-  //   res.json({ message: "success", data: results.rows });
-  // });
-})
+  const pool = new Pool();
+  pool.query(sql, (error, results) => {
+    if (error) {
+      res.statusCode = 500;
+      return res.json({errors: ['Failed to retrieve: ' + error.message]})
+    }
+    res.statusCode = 200;
+    res.json({ message: "success", data: results.rows });
+  });
+});
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-
+app.use("/.netlify/functions/server", router); // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
